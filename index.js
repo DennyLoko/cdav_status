5f75573b7db865e33700542a79d124cead6b17f8
Danniel Magno <dennyloko@gmail.com>
2021-06-19 00:08:33 -0300

    feat(app): Initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const axios = require('axios')
+
+const app = express()
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    axios.get(`http://${process.env.CDAV_ENDPOINT}/players.json`)
+        .then(resp => {
+            if (resp.status == 200) {
+                res.json({
+                    "status": "success",
+                    "message": "Successfully got server status",
+                    "server": {
+                        "players": resp.data.length,
+                        "status": "up"
+                    }
+                })
+            } else {
+                console.log(`Error: ${resp.status}`)
+
+                res.status(500).json({
+                    "status": "error",
+                    "message": "Failed to get server status",
+                    "server": {
+                        "players": 0,
+                        "status": "down"
+                    }
+                })
+            }
+        })
+        .catch(e => {
+            console.log(e.code)
+
+            res.status(500).json({
+                    "status": "error",
+                    "message": "Failed to get server status",
+                    "server": {
+                        "players": 0,
+                        "status": "down"
+                    }
+                })
+        })
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
